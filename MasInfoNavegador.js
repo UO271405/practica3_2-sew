5c1796d62fa6207dc08518b2d5bde5c6dc194aa0
Luis <UO271405@uniovi.es>
2020-12-01 23:40:00 +0100

    ficheros practica 3 ejercicio 2

---
+++ MasInfoNavegador.js
@@ -0,0 +1,15 @@
+document.write("<p>Nombre del navegador: ");
+document.write(infoNavegador.nombre);
+document.write(" - Idioma: ");
+document.write(infoNavegador.idioma);
+document.write(" - Version: ");
+document.write(infoNavegador.version);
+document.write(" - Plataforma: ");
+document.write(infoNavegador.plataforma);
+document.write(" - Vendedor: ");
+document.write(infoNavegador.vendedor);
+document.write(" - Agente: ");
+document.write(infoNavegador.agente);
+document.write(" - Java activo: ");
+document.write(infoNavegador.javaActivo);
+document.write("</p>");
